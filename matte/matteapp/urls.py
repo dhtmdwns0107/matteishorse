406be4f3579e934db4dcf8caef0f65c77084f5ef
Sean Oh <dhtmdwns0107@gmail.com>
2020-02-05 12:16:09 +0900

    1215oh

---
+++ matte/matteapp/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index2/', views.index2, name='index2'),
+]
