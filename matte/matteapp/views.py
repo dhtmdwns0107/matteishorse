a2745d8ea54e176d6dfb2c2a3156d7499bb9ef16
yonghyun Jeong <jayjay@kakao.com>
2020-02-05 18:30:12 +0900

    Show selected result from result table

--- matte/matteapp/views.py
+++ matte/matteapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
+from .models import Question, Result
 
 def index(request):
     return render(request, 'matteapp/index.html', {})
@@ -19,5 +19,12 @@ def result(request): # 투표 결과 페이지
     # question = Question.objects.get(pk=q_id) # models.py에서 이거 자체가 객체로 지정되어 있기 때문에
     # choices = question.choice_set.all()
     # choices = Choice.objects.filter(question=question) # 이건 뭔 방식이지 근데 위와 같은 방식이라고 한다
+    result_list = Result.objects.filter(test_id = 1)
+    
+    for result in result_list:
+        selected_id = result.res_id
+        if result.res_div >= int(num):
+            break
 
-    return render(request, 'matteapp/result.html', {})
+    print(selected_id)
+    return render(request, 'matteapp/result.html', {'result_list' : result_list, 'selected_id' : selected_id})
