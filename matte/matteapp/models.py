84d7cdc10bb66f03cddc4710b6c339aabf9ef261
yonghyun Jeong <jayjay@kakao.com>
2020-02-06 18:09:06 +0900

    Add function about signin/out

--- matte/matteapp/models.py
+++ matte/matteapp/models.py
@@ -1,29 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    user_pw = models.CharField(max_length=10)
-    user_name = models.CharField(max_length=10)
-    user_age = models.IntegerField(max_length=10)
-    user_sex = models.CharField(max_length=10)
-    user_add = models.CharField(max_length=30)
+# class User(models.Model):
+#     user_id = models.AutoField(primary_key=True)
+#     user_pw = models.CharField(max_length=10)
+#     user_name = models.CharField(max_length=10)
+#     user_email = models.CharField(max_length=30)
+#     user_age = models.IntegerField(max_length=10)
+#     user_sex = models.CharField(max_length=10)
+#     user_add = models.CharField(max_length=30)
 
-    class Meta():
-        db_table = 'user'
+#     class Meta():
+#         db_table = 'user'
 
-class Test(models.Model):
+class Mindtest(models.Model):
     test_id = models.AutoField(primary_key=True)
     test_text = models.CharField(max_length=40)
     test_info = models.CharField(max_length=100)
 
     class Meta():
-        db_table = 'test'
+        db_table = 'mindest'
 
 class Question(models.Model):
     q_id = models.AutoField(primary_key=True)
     q_text = models.CharField(max_length=200)
-    test = models.ForeignKey(Test, on_delete=models.CASCADE) # test -> test_id
+    test = models.ForeignKey(Mindtest, on_delete=models.CASCADE) # test -> test_id
 
     class Meta():
         db_table = 'question'
@@ -34,25 +36,25 @@ class Question(models.Model):
 class Choice(models.Model):
     c_id = models.AutoField(primary_key=True)
     c_val = models.IntegerField(max_length=11)
-    q = models.ForeignKey(Question, on_delete=models.CASCADE) # test -> test_id
+    q = models.ForeignKey(Question, on_delete=models.CASCADE) # q -> q_id
 
     class Meta():
         db_table = 'choice'
 
-class Result(models.Model):
+class Qresult(models.Model):
     res_id = models.AutoField(primary_key=True)
     res_text = models.CharField(max_length=100)
     res_title = models.CharField(max_length=500)
     res_div = models.IntegerField(max_length=11)
-    test = models.ForeignKey(Test, on_delete=models.CASCADE) # test -> test_id
+    test = models.ForeignKey(Mindtest, on_delete=models.CASCADE) # test -> test_id
 
     class Meta():
-        db_table = 'result'
+        db_table = 'qresult'
 
 class Mypage(models.Model):
     mp_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE) # user -> user_id
-    test = models.ForeignKey(Test, on_delete=models.CASCADE) # test -> test_id
+    test = models.ForeignKey(Mindtest, on_delete=models.CASCADE) # test -> test_id
     test_date = models.DateTimeField('date published')
 
     class Meta():
--- matte/matteapp/views.py
+++ matte/matteapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question, Result
+from .models import Question, Qresult
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth import logout
 
 def index(request):
     question_list = Question.objects.filter(test_id = 1)
@@ -11,9 +14,36 @@ def index2(request):
     return render(request, 'matteapp/index2.html', {})
 
 def signin(request):
-    return render(request, 'matteapp/signin.html', {})
+    if request.method == "POST":
+        
+        # email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+
+        print(username, password)
+        user = auth.authenticate(request, username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            return render(request, 'matteapp/index.html', {})
+        else:
+            return render(request, 'matteapp/signup.html', {})
+
 def signup(request):
+    if request.method == "POST":
+        user = User.objects.create_user(
+            username = request.POST['username'],
+            password = request.POST['password'],
+            email = request.POST['useremail'],
+            # user_age = request.POST['userage'],
+            # user_sex = request.POST['usersex'],
+            # user_addr = request.POST['useraddr']
+        )
+        auth.login(request, user)
     return render(request, 'matteapp/signup.html', {})
+
+def signout(request):
+    logout(request)
+    return render(request, 'matteapp/index.html', {})
 
 def test1(request):
     test_id = request.GET.get('test_id')
@@ -24,7 +54,7 @@ def result(request):
     test_id = request.GET.get('test_id')
     num = request.POST.get('total')
     print(num)
-    result_list = Result.objects.filter(test_id = test_id)
+    result_list = Qresult.objects.filter(test_id = test_id)
     
     for result in result_list:
         selected_id = result.res_id
